c13fd1afa2093a603c443accecc052503ba0d23a
joaopedromsantos <joaopedromartinscrf2019@gmail.com>
2026-03-13 00:52:05 -0300

    feat: add UserCreate and UserResponse schemas for user management

---
+++ app/schemas/user.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from uuid import UUID
+from pydantic import BaseModel, EmailStr
+from app.models.enums.pix_key_type import PixKeyType
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    name: str
+    password: str
+    pix_key: str
+    pix_key_type: PixKeyType
+
+class UserResponse(BaseModel):
+    id: UUID
+    email: str
+    name: str
+    pix_key: Optional[str]
+    pix_key_type: Optional[PixKeyType]
+
+    model_config = {"from_attributes": True}
