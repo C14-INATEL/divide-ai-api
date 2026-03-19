18e5ac95b9203e6a3fbff11a768997b0d22a3294
Henrique Pizzoni <rikepizzoni@gmail.com>
2026-03-18 21:26:18 -0300

    Update migrations/env.py
    Co-authored-by: João Pedro Santos <81121643+joaopedromsantos@users.noreply.github.com>

--- migrations/env.py
+++ migrations/env.py
@@ -5,17 +5,13 @@ from sqlalchemy import pool
 
 from alembic import context
 
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
+from app.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
-
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
