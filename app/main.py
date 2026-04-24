a0282a89518881d405af7000be8313bd07c6e7e5
joaopedromsantos <81121643+joaopedromsantos@users.noreply.github.com>
2026-04-24 00:46:14 -0300

    feat: include groups router in main application

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import users, auth
+from app.routers import users, auth, groups
 from app.database import Base, engine
 
 
@@ -9,10 +9,9 @@ app = FastAPI(
     title="DivideAi API",
     description="Documentação da API",
     version="1.0.0",
-    swagger_ui_parameters={
-        "persistAuthorization": True
-    }
+    swagger_ui_parameters={"persistAuthorization": True},
 )
 
 app.include_router(auth.router)
 app.include_router(users.router)
+app.include_router(groups.router)
